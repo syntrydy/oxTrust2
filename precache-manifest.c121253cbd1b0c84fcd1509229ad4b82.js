9fd816622e8b6f39eaf9dc2d5deed58318503b05
Gasmyr <thomas@gluu.org>
2019-10-31 14:02:26 +0100

    Updates

---
+++ precache-manifest.c121253cbd1b0c84fcd1509229ad4b82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da077585b90ed9479c877c222c426233",
+    "url": "/oxTrust2/index.html"
+  },
+  {
+    "revision": "11d27d84e47e2d06763b",
+    "url": "/oxTrust2/static/css/main.b9e16efc.chunk.css"
+  },
+  {
+    "revision": "186f118be042ad285116",
+    "url": "/oxTrust2/static/js/2.65269dae.chunk.js"
+  },
+  {
+    "revision": "11d27d84e47e2d06763b",
+    "url": "/oxTrust2/static/js/main.bca378e2.chunk.js"
+  },
+  {
+    "revision": "3a371c3bba3e8d6e9f59",
+    "url": "/oxTrust2/static/js/runtime-main.421eea49.js"
+  },
+  {
+    "revision": "82ed018016eef3a34e5554cf679752c9",
+    "url": "/oxTrust2/static/media/logo.82ed0180.png"
+  }
+]);
