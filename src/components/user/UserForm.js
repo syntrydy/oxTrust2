fc1eb96f8b45db2461e627de4e1fcc59d7cc3ed9
Gasmyr <thomas@gluu.org>
2019-11-19 15:00:54 +0100

    Fix User Add Form

--- src/components/user/UserForm.js
+++ src/components/user/UserForm.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Button from "@material-ui/core/Button";
 import SaveIcon from "@material-ui/icons/Save";
 import TextField from "@material-ui/core/TextField";
 import FormControl from "@material-ui/core/FormControl";
 import useForm from "react-hook-form";
-import Select from "@material-ui/core/Select";
+import NativeSelect from "@material-ui/core/NativeSelect";
+import InputLabel from "@material-ui/core/InputLabel";
 import Grid from "@material-ui/core/Grid";
+import InputBase from "@material-ui/core/InputBase";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -24,22 +26,64 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
+const BootstrapInput = withStyles(theme => ({
+  root: {
+    "label + &": {
+      marginTop: theme.spacing(3)
+    }
+  },
+  input: {
+    borderRadius: 4,
+    position: "relative",
+    backgroundColor: theme.palette.background.paper,
+    border: "1px solid #ced4da",
+    width: "80%",
+    fontSize: 16,
+    padding: "10px 26px 10px 12px",
+    transition: theme.transitions.create(["border-color", "box-shadow"]),
+    fontFamily: [
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"'
+    ].join(","),
+    "&:focus": {
+      borderRadius: 4,
+      borderColor: "#80bdff",
+      boxShadow: "0 0 0 0.2rem rgba(0,123,255,.25)"
+    }
+  }
+}))(InputBase);
+
 const UserForm = () => {
   const classes = useStyles();
   const { handleSubmit, register, errors } = useForm();
-  const [age, setAge] = React.useState("");
+  const [status, setStatus] = React.useState("");
   const onSubmit = values => {
     console.log(values);
   };
-  const handleChange = event => {
-    setAge(event.target.value);
+  const handleStatusChange = event => {
+    setStatus(event.target.value);
   };
 
   return (
     <Paper elevation={4}>
-      <form onSubmit={handleSubmit(onSubmit)} style={{ padding: "25px" }}>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        style={{
+          marginLeft: "10%",
+          marginRight: "10%",
+          paddingTop: "20px"
+        }}
+      >
         <Grid container spacing={0}>
-          <Grid item xs={6}>
+          <Grid item xs={12}>
             <TextField
               id="username"
               name="username"
@@ -51,7 +95,7 @@ const UserForm = () => {
             />
             {errors.groupName && "username is required"}
           </Grid>
-          <Grid item xs={6}>
+          <Grid item xs={12}>
             <TextField
               id="firstName"
               name="firstName"
@@ -62,7 +106,7 @@ const UserForm = () => {
               variant="outlined"
             />
           </Grid>
-          <Grid item xs={6}>
+          <Grid item xs={12}>
             <TextField
               id="lastName"
               name="lastName"
@@ -73,7 +117,7 @@ const UserForm = () => {
               variant="outlined"
             />
           </Grid>
-          <Grid item xs={6}>
+          <Grid item xs={12}>
             <TextField
               id="displayName"
               name="displayName"
@@ -84,7 +128,7 @@ const UserForm = () => {
               variant="outlined"
             />
           </Grid>
-          <Grid item xs={6}>
+          <Grid item xs={12}>
             <TextField
               id="email"
               name="Email"
@@ -96,16 +140,7 @@ const UserForm = () => {
               variant="outlined"
             />
           </Grid>
-          <Grid item xs={6}>
-            <FormControl className={classes.inputwidth}>
-              <Select
-                id="statusSelect"
-                onChange={handleChange}
-                value={age}
-              ></Select>
-            </FormControl>
-          </Grid>
-          <Grid item xs={6}>
+          <Grid item xs={12}>
             <TextField
               id="password"
               name="password"
@@ -116,7 +151,7 @@ const UserForm = () => {
               variant="outlined"
             />
           </Grid>
-          <Grid item xs={6}>
+          <Grid item xs={12}>
             <TextField
               id="confirmpassword"
               name="confirmpassword"
@@ -128,9 +163,26 @@ const UserForm = () => {
             />
           </Grid>
           <Grid item xs={12}>
+            <FormControl className={classes.margin} style={{ width: "200px" }}>
+              <InputLabel htmlFor="status">Status</InputLabel>
+              <NativeSelect
+                id="statusSelectBox"
+                value={status}
+                name="status"
+                inputRef={register({ required: true })}
+                onChange={handleStatusChange}
+                input={<BootstrapInput />}
+              >
+                <option value="Active">Active</option>
+                <option value="Inactive">Inactive</option>
+              </NativeSelect>
+            </FormControl>
+          </Grid>
+          <Grid item xs={12}>
             <Button
               type="submit"
               variant="contained"
+              style={{ marginTop: "3%", marginBottom: "3%" }}
               color="primary"
               startIcon={<SaveIcon />}
             >
